951086f16135af59457a7e4ac82712537bac0de2
Krzysztof Spisak-Spisacki <krzysztof.spisak.spisacki@espeo.eu>
2019-09-12 15:26:27 +0200

    Moved credts to settings + minor improvements
    Removed IAS cert (no rebase because lazy)

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ To build: `g++ main.cpp ra.a -I/opt/sgxsdk/include -L/usr/lib -lssl -lcrypto -o 
 Requires OpenSSL. ra.a is a static library built from gardener-sgx-enclave/Attestation/*.o that contains implementation of methods common for both sides of remote attestation process.
 
 It also requires `IAS signing CA certificate` put it in `ias-cert.pem` in root dir.
+Additional mandatory configuration - such as setting credentials necessary to communicate with Intel Attestation Service is done via `settings.h`
--- settings.h
+++ settings.h
@@ -28,8 +28,37 @@
 #define DEFAULT_CA_BUNDLE_AUTO "/etc/ssl/certs/ca-certificates.crt"
 #define DEFAULT_CA_BUNDLE_LINUX	DEFAULT_CA_BUNDLE_AUTO
 
-/* Default CA bundle file on Windows */
-#define DEFAULT_CA_BUNDLE_WIN32	"C:\\Program Files\\cURL\\bin\\curl-ca-bundle.crt"
+/*
+ * Intel Attestation Service  Secondary Subscription Key
+ * Acquire it using https://api.portal.trustedservices.intel.com/EPID-attestation
+ */
 
+#define IAS_PRIMARY_SUBSCRIPTION_KEY "your-primary-sub-key"
+
+/*
+ * Intel Attestation Service  Secondary Subscription Key
+ * This will be used in case the primary subscription key does not work
+ * Acquire it using https://api.portal.trustedservices.intel.com/EPID-attestation
+ */
+
+#define IAS_SECONDARY_SUBSCRIPTION_KEY "your-secondary-sub-key"
+
+// Acquire it using https://api.portal.trustedservices.intel.com/EPID-attestation
+
+#define SPID "your-spid"
+
+/*
+ * The Intel IAS SGX Report Signing CA file. You are sent this certificate
+ * when you apply for access to SGX Developer Services at
+ * http://software.intel.com/sgx [REQUIRED]
+ */
+
+#define IAS_CERT_FILENAME "ias-cert.pem"
+
+/*
+ * Identifies authority that sealed an enclave. Acquired from your enclave.
+ */
+
+#define MRSIGNER "your-mrsigner"
 
 #endif
